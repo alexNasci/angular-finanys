398457c92ee6eddeb5584b57c5127a281d8ea1d0
Alexandro Nascimento <alexandro.silva@grupotel.com.br>
2019-06-12 17:08:34 -0300

    Consumindo api que lista categorias e iniciando formulário de cadastro de categorias.

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentChecked(){
+
   }
 
 }
